6ee3ff301a9db90ed438c0dc713644bd28c00a5b
Jooye <986125495@qq.com>
2023-06-07 21:11:24 +0800

    docs(main): Nio网络编程Selector学习

--- Nio_Bio/src/test/java/com/jorry/nio/socket/MyServer2.java
+++ Nio_Bio/src/test/java/com/jorry/nio/socket/MyServer2.java
@@ -2,7 +2,9 @@ package com.jorry.nio.socket;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -11,7 +13,10 @@ import java.util.Set;
  * @date : 2023-06-06 21:52
  * @Describe: 类的描述信息
  */
+@SuppressWarnings("all")
 public class MyServer2 {
+
+
     public static void main(String[] args) throws IOException {
         ServerSocketChannel ssc = ServerSocketChannel.open();
         ssc.bind(new InetSocketAddress(8888));
@@ -30,25 +35,34 @@ public class MyServer2 {
          *    register注册ssc
          */
         selectionKey.interestOps(SelectionKey.OP_ACCEPT);
-
         System.out.println("MyServer2.main");
         //监控 管理的ServerSocketChannel
         while (true) {
             //只有监控到了有实际的连接，或者读写操作才会进行处理
             //对应的有ACCEPT状态和READ/WRITE状态的SC存起来   SelectionKeys HashSet
             selector.select();
-            System.out.println("-------------------------------");
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
+                //用完之后，就要把他从SelectedKeys集合中删除掉，问题？ServerSocketChannel----SelectedKeys删除 ，后续SSC建立新的连接
+                iterator.remove();
                 if (key.isAcceptable()) {
                     //获取ServerSocketChannel
                     ServerSocketChannel channel = (ServerSocketChannel) key.channel();
                     SocketChannel socketChannel = channel.accept();
+                    //设置SocketChannel为非阻塞的状态
+                    socketChannel.configureBlocking(false);
+                    //监控SC状态
+                    SelectionKey sk = socketChannel.register(selector, 0, null);
+                    sk.interestOps(SelectionKey.OP_READ);
                     System.out.println("socketChannel = " + socketChannel);
                 } else if (key.isReadable()) {
-
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    ByteBuffer buffer = ByteBuffer.allocate(20);
+                    channel.read(buffer);
+                    buffer.flip();
+                    System.out.println("Charset.defaultCharset().decode(buffer).toString() = " + Charset.defaultCharset().decode(buffer).toString());
                 }
             }
         }
